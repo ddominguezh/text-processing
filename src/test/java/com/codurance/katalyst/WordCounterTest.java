cefdf1699dfdc667cb95f7bae1a7d1caaa0b1f9f
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-13 22:42:44 +0200

    create test: ignore consecutive whitespace

--- src/test/java/com/codurance/katalyst/WordCounterTest.java
+++ src/test/java/com/codurance/katalyst/WordCounterTest.java
@@ -25,4 +25,14 @@ public class WordCounterTest {
     public void ignore_symbols(){
         assertEquals(0, WordCounter.create("(·%*` ").count());
     }
+
+    @Test
+    public void counts_words_from_text(){
+        assertEquals(21, WordCounter.create("Hello, this is an example for you to practice. You should grab this text and make it as your test case.").count());
+    }
+
+    @Test
+    public void ignore_empty_words_from_text(){
+        assertEquals(21, WordCounter.create("Hello, this  is an example   for  you to practice. You should grab this text and make it as your test case.").count());
+    }
 }
