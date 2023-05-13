73c30b6690175e3ff29582b5f3f4e66711b42641
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-13 21:09:03 +0200

    create test: remove dots from text

---
+++ src/main/java/com/codurance/katalyst/TextCleaner.java
@@ -0,0 +1,15 @@
+package com.codurance.katalyst;
+
+public class TextCleaner {
+
+    private String text;
+    protected TextCleaner(String text){
+        this.text = text;
+    }
+    public static TextCleaner create(String text) {
+        return new TextCleaner(text);
+    }
+    public String clean(){
+        return null;
+    }
+}
--- src/test/java/com/codurance/katalyst/TextCleanerTest.java
+++ src/test/java/com/codurance/katalyst/TextCleanerTest.java
@@ -1,5 +1,13 @@
 package com.codurance.katalyst;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TextCleanerTest {
     
+    @Test
+    public void remove_dots(){
+        assertEquals("Hello, this is an example for you to practice You should grab this text and make it as your test case", TextCleaner.create("Hello, this is an example for you to practice. You should grab this text and make it as your test case.").clean());
+    }
 }
