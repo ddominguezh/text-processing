1cfb3ff1b2778823ded660be21882c295746a507
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-14 17:55:48 +0200

    create test: remove javascript code

--- src/test/java/com/codurance/katalyst/TextCleanerTest.java
+++ src/test/java/com/codurance/katalyst/TextCleanerTest.java
@@ -20,4 +20,10 @@ public class TextCleanerTest {
     public void remove_commas_and_dots_and_brackets(){
         assertEquals("Hello this is an example for you to practice You should grab this text and make it as your test case", TextCleaner.create("Hello, this is an example for you to practice. You should grab this (text) and make it as your test case.").clean());
     }
+
+    @Test
+    public void remove_javascript_code(){
+        assertEquals("Hello this is an example for you to practice You should grab this text and make it as your test case", TextCleaner.create("Hello, this is an example for you to practice. <javascript>if (true) {console.log('should should should')}</javascript> You should grab this (text) and make it as your test case.").clean());
+    }
+    
 }
