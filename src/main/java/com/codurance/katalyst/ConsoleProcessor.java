c992c994b8a18df1452b686becd03e78e8ad9d8b
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-14 18:45:52 +0200

    print analysis

--- src/main/java/com/codurance/katalyst/ConsoleProcessor.java
+++ src/main/java/com/codurance/katalyst/ConsoleProcessor.java
@@ -2,10 +2,21 @@ package com.codurance.katalyst;
 
 public class ConsoleProcessor implements Processor{
 
+    private static final int MAX_TOP_RESULTS = 10;
     @Override
     public void analyse(String text) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'analyse'");
+        System.out.println("Those are the top " + MAX_TOP_RESULTS + " words used:");
+        System.out.println("");
+
+        int index = 1;
+        for (String word : TopWords.create(text).max(MAX_TOP_RESULTS)) {
+            System.out.println(index + ". " + word);
+            index++;
+        }
+
+        System.out.println("");
+        System.out.println("The text has in total " + WordCounter.create(text).count() + " words");
+        
     }
     
 }
