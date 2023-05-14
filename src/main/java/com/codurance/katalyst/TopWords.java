4b5ec3fd22c36dbe140697e2222162dca2a01163
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-05-14 08:12:24 +0200

    clean text, split by whitespace and sorted stream

--- src/main/java/com/codurance/katalyst/TopWords.java
+++ src/main/java/com/codurance/katalyst/TopWords.java
@@ -1,6 +1,8 @@
 package com.codurance.katalyst;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TopWords {
 
@@ -9,10 +11,13 @@ public class TopWords {
         this.text = text;
     }
     public static TopWords create(String text) {
-        return new TopWords(text);
+        return new TopWords(TextCleaner.create(text).clean());
     }
     public List<String> value() {
-        return null;
+        return Arrays.asList(text.split(" ", -1))
+            .stream()
+            .sorted((a, b) -> a.toUpperCase().compareTo(b.toUpperCase()))
+            .collect(Collectors.toList());
     }
     
 }
